4843a1d550e86619eb61a4da096bacb4cf3a8a95
firman ramadhan <firmanramadhan066@gmail.com>
2024-10-16 17:10:49 +0700

    update

--- edit.php
+++ edit.php
@@ -18,7 +18,7 @@ $jadwal = getJadwalById($_GET['id']);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../jadwal-pelajaran/css/style.css">
+    <link rel="stylesheet" href="style.css">
     <title>Edit Jadwal</title>
 </head>
 <body>
